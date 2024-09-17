7238ee0ccbf06c7c3c381426eccdffbb6bb316df
ppfon <73702392+ppfon@users.noreply.github.com>
2024-09-17 13:26:50 -0300

    Adição da equação nos terminais do conversor

--- 4_Teste de estratégias/estratégias/calcula_espectro.m
+++ 4_Teste de estratégias/estratégias/calcula_espectro.m
@@ -5,6 +5,9 @@ function [amp,fase, freq] = calcula_espectro(amp_sinal, periodo, pu)
     Y = abs(fft(amp_sinal, num_pontos))/num_pontos;
 
     espectro = Y(1:num_pontos/2+1);
+    
+    Y_pot = fft(amp_sinal, num_pontos) 
+    pot_espectro = (1/(freq_amostragem * num_pontos)) * 
     espectro(2:end-1) = 2*espectro(2:end-1);
     
    if (pu) 
--- 4_Teste de estratégias/estratégias/plota_resultado.m
+++ 4_Teste de estratégias/estratégias/plota_resultado.m
@@ -2,53 +2,53 @@ freq_amostragem = 9e3;
 periodo_amostragem = 1/freq_amostragem;
 resultado_pu = 0;
 
-le_base_ativo = load("Novos dados\bpsc\q2000p0\ativo\base.mat", "stgy");
-base_ativo = le_base_ativo.stgy;
+le_base_ativa = load("Novos dados\apoc\ativa\base.mat", "base");
+base_ativa = le_base_ativa.base;
 
-le_stgy_ativo = load("Novos dados\bpsc\q2000p0\ativo\stgy.mat", "stgy");
-stgy_ativo = le_stgy_ativo.stgy;
+le_stgy_ativa = load("Novos dados\apoc\ativa\stgy.mat", "stgy");
+stgy_ativa = le_stgy_ativa.stgy;
 
-amplitude_stgy_ativo = stgy_ativo(:,2);
-amplitude_base_ativo = base_ativo(:,2);
+amplitude_stgy_ativa = stgy_ativa(:,2);
+amplitude_base_ativa = base_ativa(:,2);
 
-le_base_reativo = load("Novos dados\bpsc\q2000p0\reativo\base.mat", "stgy");
-base_reativo = le_base_reativo.stgy;
+le_base_reativa = load("Novos dados\apoc\reativa\base.mat", "base");
+base_reativa = le_base_reativa.base;
 
-le_stgy_reativo = load("Novos dados\bpsc\q2000p0\reativo\stgy.mat", "stgy");
-stgy_reativo = le_stgy_reativo.stgy;
+le_stgy_reativa = load("Novos dados\apoc\reativa\stgy.mat", "stgy");
+stgy_reativa = le_stgy_reativa.stgy;
 
-amplitude_stgy_reativo = stgy_reativo(:,2);
-amplitude_base_reativo = base_reativo(:,2);
+amplitude_stgy_reativa = stgy_reativa(:,2);
+amplitude_base_reativa = base_reativa(:,2);
 
 figure(1);
 
 subplot(1,2,1);
-[amp, fase, freq] = calcula_espectro(amplitude_base_ativo, periodo_amostragem, resultado_pu);
+[amp, fase, freq] = calcula_espectro(amplitude_base_ativa, periodo_amostragem, resultado_pu);
 plot(freq, amp, 'r');
-title("Potência ativo");
+title("Potência ativa");
 xlabel('Frequência [Hz]'); ylabel('Amplitude [W]');xlim([0 600]);
 grid on; 
 hold on;
 %subplot(2,2,2);
-[amp, fase, freq] = calcula_espectro(amplitude_stgy_ativo, periodo_amostragem, resultado_pu);
+[amp, fase, freq] = calcula_espectro(amplitude_stgy_ativa, periodo_amostragem, resultado_pu);
 plot(freq, amp, 'b'); 
-%title("Potência ativo - com a estratégia");
+%title("Potência ativa - com a estratégia");
 %xlabel('Frequência [Hz]'); ylabel('Amplitude [W]');
 %grid on;
 legend('APOC | P_{ref} = 2kW | Sem estratégia', 'APOC | P_{ref} = 2kW | Com estratégia')
 hold off;
 
 subplot(1,2,2)
-[amp, fase, freq] = calcula_espectro(amplitude_base_reativo, periodo_amostragem, resultado_pu);
+[amp, fase, freq] = calcula_espectro(amplitude_base_reativa, periodo_amostragem, resultado_pu);
 plot(freq, amp, 'r');
-title("Potência reativo");
+title("Potência reativa");
 xlabel('Frequência [Hz]'); ylabel('Amplitude [VAr]'); xlim([0 600]);
 grid on; hold on;
 
 %subplot(2,2,4);
-[amp, fase, freq] = calcula_espectro(amplitude_stgy_reativo, periodo_amostragem, resultado_pu);
+[amp, fase, freq] = calcula_espectro(amplitude_stgy_reativa, periodo_amostragem, resultado_pu);
 plot(freq, amp, 'b'); 
-% title("Potência Reativo - com a estratégia");
+% title("Potência Reativa - com a estratégia");
 % xlabel('Frequência [Hz]'); ylabel('Amplitude [VAr]');
 % grid on;
 legend('APOC | Q_{ref} = 1kVAr | Sem estratégia', 'APOC | Q_{ref} = 1kVAr | Com estratégia')
@@ -56,22 +56,22 @@ hold off;
 
 
 figure(2);
-tempo_base_ativo = (0:length(amplitude_base_ativo)-1)/freq_amostragem;
-tempo_stgy_ativo = (0:length(amplitude_stgy_ativo)-1)/freq_amostragem;
+tempo_base_ativa = (0:length(amplitude_base_ativa)-1)/freq_amostragem;
+tempo_stgy_ativa = (0:length(amplitude_stgy_ativa)-1)/freq_amostragem;
 
-tempo_base_reativo = (0:length(amplitude_base_reativo)-1)/freq_amostragem;
-tempo_stgy_reativo = (0:length(amplitude_stgy_reativo)-1)/freq_amostragem;
+tempo_base_reativa = (0:length(amplitude_base_reativa)-1)/freq_amostragem;
+tempo_stgy_reativa = (0:length(amplitude_stgy_reativa)-1)/freq_amostragem;
 
 subplot(1,2,1);
-plot(tempo_base_ativo, amplitude_base_ativo, 'r');
-title("Potência ativo");
+plot(tempo_base_ativa, amplitude_base_ativa, 'r');
+title("Potência ativa");
 xlabel('Tempo [s]'); ylabel('Amplitude [W]');
 grid on; hold on; xlim([0 0.025]); 
 
 
 % subplot(2,2,2);
-plot(tempo_stgy_ativo, amplitude_stgy_ativo, 'b'); 
-% title("Potência ativo - com a estratégia");
+plot(tempo_stgy_ativa, amplitude_stgy_ativa, 'b'); 
+% title("Potência ativa - com a estratégia");
 % xlabel('Tempo [s]'); ylabel('Amplitude [W]');
 % grid on;
 legend('APOC | P_{ref} = 2kW | Sem estratégia', 'APOC | P_{ref} = 2kW | Com estratégia');
@@ -79,14 +79,14 @@ legend('APOC | P_{ref} = 2kW | Sem estratégia', 'APOC | P_{ref} = 2kW | Com est
 hold off;
 
 subplot(1,2,2)
-plot(tempo_base_reativo, amplitude_base_reativo, 'r');
-title("Potência reativo");
+plot(tempo_base_reativa, amplitude_base_reativa, 'r');
+title("Potência reativa");
 xlabel('Tempo [s]'); ylabel('Amplitude [VAr]');
 grid on; hold on; xlim([0 0.025]);
 % 
 % subplot(2,2,4);
-plot(tempo_stgy_reativo, amplitude_stgy_reativo, 'b'); 
-% title("Potência Reativo - com a estratégia");
+plot(tempo_stgy_reativa, amplitude_stgy_reativa, 'b'); 
+% title("Potência Reativa - com a estratégia");
 % xlabel('Tempo [s]'); ylabel('Amplitude [VAr]');
 legend('APOC | Q_{ref} = 1kVAr | Sem estratégia', 'APOC | Q_{ref} = 1kVAr | Com estratégia');
 hold off;
